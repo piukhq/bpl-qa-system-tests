99189e2cc19af47ceb239f638961291c0d1c1b8c
bhaskermamidisetti <bmamidisetti@bink.com>
2021-03-29 16:16:44 +0100

    Completed automation scripting gor BPL sprint1 stories

--- tests/customer_management_api/requests/enrolment.py
+++ tests/customer_management_api/requests/enrolment.py
@@ -1,6 +1,7 @@
 import json
+import logging
 
-from tests.customer_management_api.requests.base import get_url, Endpoints, get_headers
+from tests.customer_management_api.requests.base import get_url, Endpoints, get_headers, get_invalid_headers
 from tests.retry_requests import retry_session
 
 
@@ -8,4 +9,21 @@ def send_post_enrolment(retailer_slug, request_body):
     url = get_url(retailer_slug, Endpoints.ENROL)
     headers = get_headers()
     session = retry_session()
+    logging.info("POST enrol URL is :" + url)
+    return session.post(url, headers=headers, data=json.dumps(request_body))
+
+
+def send_malformed_enrolment(retailer_slug, request_body):
+    url = get_url(retailer_slug, Endpoints.ENROL)
+    headers = get_headers()
+    session = retry_session()
+    logging.info("POST enrol URL is :" + url)
+    return session.post(url, headers=headers, data=request_body)
+
+
+def send_Invalid_post_enrolment(retailer_slug, request_body):
+    url = get_url(retailer_slug, Endpoints.ENROL)
+    headers = get_invalid_headers()
+    session = retry_session()
+    logging.info("POST enrol URL is :" + url)
     return session.post(url, headers=headers, data=json.dumps(request_body))
