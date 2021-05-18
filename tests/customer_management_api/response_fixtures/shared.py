e6b7dca5a499e8a42501a11d6ca17748773294ca
Francesco Milani <fmilani@bink.com>
2021-05-18 18:43:36 +0300

    revert timezone change

--- tests/customer_management_api/response_fixtures/shared.py
+++ tests/customer_management_api/response_fixtures/shared.py
@@ -1,3 +1,4 @@
+from datetime import timezone
 from typing import TYPE_CHECKING
 
 from tests.customer_management_api.db_actions.account_holder import get_account_holder_by_id
@@ -10,7 +11,7 @@ if TYPE_CHECKING:
 
 def account_holder_details_response_body(db_session: "Session", account_holder_id: "UUID") -> dict:
     account_holder = get_account_holder_by_id(db_session, account_holder_id)
-    created_date_timestamp = int(account_holder.created_at.timestamp())
+    created_date_timestamp = int(account_holder.created_at.replace(tzinfo=timezone.utc).timestamp())
     account_holder_balances = [balance for balance in account_holder.current_balances.values()]
     for balance in account_holder_balances:
         balance["value"] = balance["value"] / 100
