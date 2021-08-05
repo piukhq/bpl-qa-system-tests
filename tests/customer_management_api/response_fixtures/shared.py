93b2b3f63bd42536b61a1c72dd356c0078901f42
Stewart Perrygrove <sperrygrove@bink.com>
2021-08-05 12:10:26 +0100

    BPL-212 drop created_date from account payloads

--- tests/customer_management_api/response_fixtures/shared.py
+++ tests/customer_management_api/response_fixtures/shared.py
@@ -1,4 +1,3 @@
-from datetime import timezone
 from typing import TYPE_CHECKING
 
 from tests.customer_management_api.db_actions.account_holder import get_account_holder_by_id
@@ -11,14 +10,12 @@ if TYPE_CHECKING:
 
 def account_holder_details_response_body(polaris_db_session: "Session", account_holder_id: "UUID") -> dict:
     account_holder = get_account_holder_by_id(polaris_db_session, account_holder_id)
-    created_date_timestamp = int(account_holder.created_at.replace(tzinfo=timezone.utc).timestamp())
     account_holder_balances = [balance for balance in account_holder.current_balances.values()]
     for balance in account_holder_balances:
         balance["value"] = balance["value"] / 100
     return {
         "UUID": str(account_holder.id),
         "email": account_holder.email,
-        "created_date": created_date_timestamp,
         "status": account_holder.status.lower(),
         "account_number": account_holder.account_number,
         "current_balances": account_holder_balances,
