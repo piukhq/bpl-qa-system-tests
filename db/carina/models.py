b55840b3ffa8b3ba564f20ec6dde91f8cdd13fc3
Stewart Perrygrove <sperrygrove@bink.com>
2021-11-03 10:13:40 +0000

    BPL-284 Update tests for separate activation and callback tasks

--- db/carina/models.py
+++ db/carina/models.py
@@ -1,10 +1,9 @@
 from sqlalchemy.ext.automap import automap_base
-from retry_tasks_lib.db.models import load_models_to_metadata
+
 from db.carina.session import engine
 
 Base = automap_base()
 Base.prepare(engine, reflect=True)
-load_models_to_metadata(Base.metadata)
 
 # get models from Base mapping
 Voucher = Base.classes.voucher
