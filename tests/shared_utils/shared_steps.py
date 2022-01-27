0e14c0dd6cda0502f5db872e563466e7c46393c8
Stewart Perrygrove <sperrygrove@bink.com>
2022-01-27 14:24:05 +0000

    Make enrolment tests pass

--- tests/shared_utils/shared_steps.py
+++ tests/shared_utils/shared_steps.py
@@ -85,7 +85,7 @@ def setup_account_holder(status: str, retailer_slug: str, request_context: dict,
 def check_response_status_code(status_code: int, request_context: dict) -> None:
     resp = request_context["response"]
     logging.info(
-        f"Response HTTP status code: {resp.status_code}" f"Response status :{json.dumps(resp.json(), indent=4)}"
+        f"Response HTTP status code: {resp.status_code} Response status: {json.dumps(resp.json(), indent=4)}"
     )
     assert resp.status_code == status_code
 
