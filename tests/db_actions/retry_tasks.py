ea9c598a0a22477894d3b2c605d20e3ceb4614e5
Haffi Mazhar <haffimazhar96@gmail.com>
2022-04-28 10:26:06 +0100

    BPL-302-Enrol-callback-success

--- tests/db_actions/retry_tasks.py
+++ tests/db_actions/retry_tasks.py
@@ -1,30 +1,23 @@
 import time
 
-from typing import TYPE_CHECKING, Union
-from uuid import UUID
+from typing import TYPE_CHECKING
 
-from retry_tasks_lib.db.models import RetryTask, RetryTaskStatuses, TaskType, TaskTypeKey, TaskTypeKeyValue
+from retry_tasks_lib.db.models import RetryTask, RetryTaskStatuses, TaskType
 from sqlalchemy import select
 
 if TYPE_CHECKING:
     from sqlalchemy.orm import Session
 
 
-def get_latest_callback_task_for_account_holder(
-    polaris_db_session: "Session", account_holder_uuid: Union[str, UUID]
-) -> RetryTask:
+def get_latest_callback_task_for_account_holder(polaris_db_session: "Session") -> RetryTask:
+    task_name = "enrolment-callback"
+    task_type = get_task_type_from_task_name(polaris_db_session, task_name)
     for i in (1, 3, 5, 10):
         time.sleep(i)
         callback_task = (
             polaris_db_session.execute(
                 select(RetryTask)
-                .where(
-                    RetryTask.task_type_id == TaskType.task_type_id,
-                    TaskType.name == "enrolment-callback",
-                    TaskTypeKeyValue.task_type_key_id == TaskTypeKey.task_type_key_id,
-                    TaskTypeKeyValue.value == str(account_holder_uuid),
-                    RetryTask.retry_task_id == TaskTypeKeyValue.retry_task_id,
-                )
+                .where(RetryTask.task_type_id == task_type.task_type_id)
                 .order_by(RetryTask.created_at.desc())
             )
             .scalars()
--- tests/features/trenette/enrol_activation_and_callback.feature
+++ tests/features/trenette/enrol_activation_and_callback.feature
@@ -1,15 +1,19 @@
-# Created by rupalpatel at 16/02/2022
-@enrol_callback
+# Created by rupalpatel at 16/02/2022, last updated by Haffi Mazhar at 03/05/2022
 Feature: Bink BPL - Ensure a customer can enrol succesfully and that activation and callback is completed
   As a customer
   I want to utilise POST enrol enodpoint
   So I can access to customer management system
 
-  Scenario: Enrol account holder successfull with callback
-
+  Background:
     Given the trenette retailer exists
-    And that retailer has the standard campaigns configured
+    And the retailer's trenette-active-campaign STAMPS campaign starts 10 days ago and ends in a day and is ACTIVE
+
+  @bpl @enrol_callback @bpl-302
+  Scenario: Enrol account holder successfull with callback
     When i Enrol a account holder passing in all required and all optional fields
     Then i receive a HTTP 202 status code response
     And the account holder is activated
+    And new enrolled account holder's trenette-active-campaign balance is 0
     And an enrolment callback task is saved in the database
+    And the enrolment-callback task status is success
+    And the balance shown for account holder is 0
